160105c8c8a9b01902a4646bf85e429ad938bb00
Richard <richarescobar@gmail.com>
2026-03-21 08:11:30 -0600

    iOS-find-Elements

---
+++ test/specs/ios-findElements.spec.js
@@ -0,0 +1,8 @@
+describe('IOS Find Elements', () => {
+    it('find element by accessibility id', async () => {
+        await $('~Alert Views').click();
+
+        await $('~Simple').click();
+        await expect(await driver.getAlertText()).toContain('A Short Title Is Best');
+    });
+});
--- test/specs/ios.spec.js
+++
@@ -1,5 +0,0 @@
-describe('IOS App', () => {
-    it('test', () => {
-
-    });
-});
